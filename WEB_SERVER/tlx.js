e5b1a5fbcf4449bf406a21c13ae6a8618945e006
paulcrepin <mail@paulcrepin.com>
2020-10-11 01:06:36 +0200

    Update : WEB_SERVER version => Join rules module & weight module + design

--- WEB_SERVER/tlx.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-/* Variable de stockage dynamique des scores */
-
-var criterias = ["Mental", "Physical", "Temporal", "Performance", "Effort", "Frustration"];
-var scores = {Mental:"None", Physical:"None", Temporal:"None", Performance:"None", Effort:"None", Frustration:"None"};
-var weightScores = {Mental:0, Physical:0, Temporal:0, Performance:0, Effort:0, Frustration:0};
--- WEB_SERVER/uiManager.js
+++ WEB_SERVER/uiManager.js
@@ -3,10 +3,13 @@
 
 // variable de stockage des object contenant chaque règles
 var ruleObjects;
-// choosen criteria = settled to first at the begining
-var selectedCriteria = "First";
 // Selection du langage par defaut
 var _selectedLangageContent;
+var _selectedOrdersContent;
+// selected criterias for the test
+var criterias;
+// choosen criteria = settled to first at the begining
+var selectedCriteria = "None";
 
 
 /******************************************************************************* INIT */
@@ -16,6 +19,8 @@ _init();
 function _init(){
   showHomepage();
   _selectedLangageContent = _FRcontent;
+  _selectedOrdersContent = _FRorder;
+  criterias = defaultCriterias;
 }
 
 
@@ -32,7 +37,7 @@ function showHomepage(){
   });
   document.getElementById("longButton").addEventListener("click", function(){
     document.body.style.background = "white";
-    showWeightComparaison()
+    loadNewTest("long")
   });
   // additional UI
   document.getElementById("HEADER").innerHTML = "";
@@ -46,6 +51,7 @@ function showHomepage(){
 function loadNewTest(version="short"){
   //clear div container
   document.getElementById("CONTAINER").innerHTML = "";
+  document.getElementById("HEADER").style.background = "black";
   /* initiation des objet règle */
   let mental_rule = new tlxRules(criterias[0]);
   let physical_rule = new tlxRules(criterias[1]);
@@ -53,78 +59,56 @@ function loadNewTest(version="short"){
   let performance_rule = new tlxRules(criterias[3]);
   let effort_rule = new tlxRules(criterias[4]);
   let frustration_rule = new tlxRules(criterias[5]);
+  // Enregistrement dans une liste
   ruleObjects = {Mental:mental_rule, Physical:physical_rule, Temporal:temporal_rule, Performance:performance_rule, Effort:effort_rule, Frustration:frustration_rule};
-  if (version == "long"){
-    /* initiation de la version longue */
-  }
-  showRules();
+  // Initiation ui de la page
+  showRules(version);
 }
 
-function showRules(){
+function showRules(version){
+  // affichage ui de la consigne
+  buildAdviceElement(document.getElementById("CONTAINER"), _selectedOrdersContent["rulesAdvice"])
+  // création de toutes les règles curseurs
   for (var i =0; i<criterias.length; i++){
     buildRuleElement(document.getElementById("CONTAINER"), criterias[i], _selectedLangageContent, ruleObjects)
   }
-  btnChannelToPython = document.createElement("BUTTON");
-  btnChannelToPython.innerHTML = "SEND";
-  document.getElementById("CONTAINER").appendChild(btnChannelToPython);
-  btnChannelToPython.addEventListener("click", function(){sendDatasToPython()});
-  /*
-  for (let i = 0; i < 6; i++) {
-    rules[i].buildRules(document.getElementById("CONTAINER"));
+  // Si c'est une version longue
+  if (version == "long"){
+    // création btn next
+    buildNextBtn(document.getElementById("CONTAINER"), _selectedOrdersContent["nextBtn"], showWeightComparaison)
   }
-  */
+  // Si c'est une version courte
+  else{
+    buildNextBtn(document.getElementById("CONTAINER"), _selectedOrdersContent["nextBtn"], finished)
+    //btnNext.addEventListener("click", function(){sendDatasToPython()});
+  }
+  // ! a remplacer
+  function finished(){console.log("finish")}
 }
 
-var feedback_scores = null; // ! possible de suprimer??
-// Fonction d'envoie des donnée à python
-function sendDatasToPython(){
-  // Création du channel
-  new QWebChannel(qt.webChannelTransport, function(channel) {
-    // Construction de l'object "channel"
-    feedback_scores = channel.objects.feedback_scores;
-    // Fonction d'envoi des données
-    feedback_scores.getRef(JSON.stringify(scores));
-  });
-}
-/******************************************************************************* Score Calculation */
-// Create score button
-addElement("div", document.getElementById("CONTAINER"), "weightChoice_score", "None", "Score");
-buttonScore = document.getElementById("weightChoice_score");
-buttonScore.addEventListener("click", function(){
-// Assign score brut and weight to variables
-    var scoreBrut = [45, 80, 25, 70, 100, 15];
-    var scoreWeight = [0, 3, 4, 3, 3, 2];
-// Merge score brut and weight in one array
-    var totalScore = mergeScore(scoreBrut, scoreWeight);
-    console.log(totalScore);
-// NASA TLX score calculation
-    var result = scoreCalculation(totalScore);
-    console.log(result);
-  }
-);
 
 
 /******************************************************************************* WEIGHT PAGE */
 
 function showWeightComparaison(){
-
   //clear div container
   document.getElementById("CONTAINER").innerHTML = "";
-
   // Creation of the list of pairs and shuffle
   var allPairs = findAllPair(criterias);
   allPairs = shuffle(allPairs);
-
   // counter made to show all pair from allPairs list
   var pairCounter = 0;
-
+  // Build UI advice
+  buildAdviceElement(document.getElementById("CONTAINER"), _selectedOrdersContent["weightAdvice"])
   // Creation of container for criterias and submit button
   addElement("div", document.getElementById("CONTAINER"), "weightChoice_container");
-  addElement("div", document.getElementById("CONTAINER"), "weightChoice_submit", "None", "Confirmer");
-  document.getElementById("weightChoice_submit").addEventListener("click", function(){clickOnNextPair()});
-
+  // Build btn
+  buildNextBtn(document.getElementById("CONTAINER"), _selectedOrdersContent["nextBtn"], clickOnNextPair, "weight")
+  // show the first comparaison
+  showPairs(allPairs, pairCounter);
   // click function to increment the counter and show the next pair
   function clickOnNextPair(){
+    console.log(selectedCriteria)
     if(selectedCriteria != "None"){
       // icrement counter
       pairCounter++;
@@ -136,14 +120,13 @@ function showWeightComparaison(){
       console.log(selectedCriteria)
       alert("Veuillez sélectionner un choix...");
     }
-
     // re-initialize value of selected criteria
     selectedCriteria = "None"
   }
   // Show a pair from 'mylist' at 'myIndex'
   function showPairs(myList, myIndex){
     // If it's not the first item
-    if (selectedCriteria != "First"){
+    if (selectedCriteria != "None"){
       weightScores[selectedCriteria]+=1;
     }
     // delete content of the module container
@@ -151,16 +134,11 @@ function showWeightComparaison(){
     // if all the pairs isn't showed
     if(pairCounter<allPairs.length){
       // build a new pairs comparaison
-      buildWeightModule(document.getElementById("weightChoice_container"), myList[myIndex]);
+      buildWeightModule(document.getElementById("weightChoice_container"), myList[myIndex], [_selectedLangageContent[myList[myIndex][0]]["title"], _selectedLangageContent[myList[myIndex][1]]["title"]]);
     }
     else{
       console.log(weightScores);
       alert("test terminé");
     }
   }
-
-
-  // show the first comparaison
-  showPairs(allPairs, pairCounter);
-
 }
